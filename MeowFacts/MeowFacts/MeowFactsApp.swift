0245d6e9dc187dd9214c824c81e3a23ae964a1bd
Anna Plotnikova <baebaeannieee@gmail.com>
2024-07-30 15:47:11 +0700

    init

---
+++ MeowFacts/MeowFacts/MeowFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowFactsApp.swift
+//  MeowFacts
+//
+//  Created by Anna on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeowFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
